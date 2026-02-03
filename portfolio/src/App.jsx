4802ac3050abbb0fe8ebe8aea0c297648d708048
Your Name <your-email@example.com>
2026-02-03 22:56:52 +0530

    Prepare for deployment: Switch to HashRouter

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Certificates from "./pages/Certificates";
-import { profile } from "./data/content";
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Certificates from "./pages/Certificates";
