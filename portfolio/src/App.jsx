b7bd2c145d8dfef12bbba14d48b6a54527ab8f6c
Your Name <your-email@example.com>
2026-02-11 11:15:01 +0530

    redy to deploy

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -23,6 +23,7 @@ export default function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/certificates" element={<Certificates />} />
+        
           </Routes>
         </Suspense>
 
